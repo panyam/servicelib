d0b3bd042f22b8d92217620dd135a7288e8a74b7
Sriram Panyam <spanyam@linkedin.com>
2017-12-09 15:10:02 -0800

    Adding date ensurer utility

--- servicelib/utils.py
+++ servicelib/utils.py
@@ -3,3 +3,10 @@ import os
 
 def is_dev_mode():
     return os.environ.get('APPLICATION_ID').startswith("dev~")
+
+DEFAULT_DATE_FORMAT = "%Y-%m-%d"
+
+def ensure_date(date_format = DEFAULT_DATE_FORMAT):
+    import datetime
+    def ensurer(strinput):
+        return datetime.datetime.strptime(date_format)
