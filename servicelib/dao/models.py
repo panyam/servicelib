a548749e71c9ba42df69b5bda91d1dc0f9616fb3
Sriram Panyam <sri.panyam@gmail.com>
2018-07-05 13:31:30 -0700

    Refactoring model bases

--- servicelib/dao/models.py
+++ servicelib/dao/models.py
@@ -1,37 +1,21 @@
 
-import datetime
-from google.appengine.ext import ndb
-from google.appengine.api import memcache
-from google.appengine.api import users
-import errors
-
-class ModelBase(ndb.Model):
+class BaseModel(object):
+    """ Base interface of all DAO models. """
     @classmethod
     def delete_all(cls):
-        keys = cls.query().fetch(1000, keys_only = True)
-        while keys:
-            ndb.delete_multi(keys)
-            keys = cls.query().fetch(1000, keys_only = True)
+        """ Deletes all instances of this model from the underlying store. """
+        raise Exception("Not Implemented")
 
     @classmethod
     def getraw(cls, obj_or_id, nothrow = False, cachekey = None):
-        obj = obj_or_id
-        if not issubclass(obj.__class__, ModelBase):
-            if issubclass(obj_or_id.__class__, ndb.Key):
-                obj = cls.get_by_id(obj_or_id.id())
-            else:
-                obj = None
-                if cachekey is not None:
-                    obj = memcache.get(cachekey)
-                if obj is None:
-                    obj = cls.get_by_id(obj_or_id)
-            if obj is None:
-                if nothrow: return None
-                else: raise errors.NotFound("Object not found for ID: " + str(obj_or_id))
-        return obj
+        """ A raw loader method for an object of this model given its ID. """
+        raise Exception("Not Implemented")
 
     def getid(self):
-        return self.key.id()
+        """ Get's the object's ID. """
+        raise Exception("Not Implemented")
 
     def delete(self):
-        ndb.delete_multi([self.key])
+        """ Kick's off a deletion of this object. """
+        raise Exception("Not Implemented")
+
