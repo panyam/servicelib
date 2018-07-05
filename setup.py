b62e4ddd85d32b39f8ab2037b2eee477c823d538
Sriram Panyam <sri.panyam@gmail.com>
2018-07-05 14:46:56 -0700

    Starting with setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ServiceLib',
+      version='1.0',
+      description='Library for writing restful services on top of all backend providers',
+      author='Sriram Panyam',
+      author_email='sri.panyam@gmail.com',
+      url='https://www.github.com/panyam/servicelib/',
+      packages=['servicelib'],
+     )
